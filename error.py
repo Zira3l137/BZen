d407d23f356ba7a78b0e67e77001499593cae873
Zira3l137 <raulbairamov2002@gmail.com>
2025-07-22 11:32:29 +0400

    feat: add if_certain_err method

--- error.py
+++ error.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Callable, Generic, Optional, Self, TypeVar
+from typing import Callable, Generic, Optional, TypeVar
 
 T = TypeVar("T")
 T2 = TypeVar("T2")
