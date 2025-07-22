6740e0267296b4c6b0ffa0ab31c40db651d7ddae
Zira3l137 <raulbairamov2002@gmail.com>
2025-07-22 11:32:56 +0400

    feat: add more specific exceptions

--- exceptions.py
+++ exceptions.py
@@ -6,3 +6,18 @@ class InvalidFileException(Exception):
 class InvalidGameDirectoryException(Exception):
     def __init__(self, message: str):
         super().__init__(message)
+
+
+class VobHasNoMeshException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class NoVisualDataException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class UnknownExtensionException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
